7c81ab79abc64833742403abbc93127f48aafd7f
C00206286 <C00206286@itcarlow.ie>
2019-01-25 10:55:33 +0000

    Stop drawing boxes

--- src/tutorial.js
+++ src/tutorial.js
@@ -230,7 +230,6 @@ class Tutorial {
     }
     document.body.style.background = "#ffffff";
     this.levelPart1.draw();
-    gameNs.game.collisionManager.render(gameNs.game.ctx);
     this.car.draw();
     this.npcManager.draw();
     this.score_text.drawText();
